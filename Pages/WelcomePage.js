7f85459865b4fd1de38693368fb5032e1b499a08
Slicknick300 <116517655+Slicknick300@users.noreply.github.com>
2022-11-30 15:28:18 -0500

    Create WelcomePage.js

---
+++ Pages/WelcomePage.js
@@ -0,0 +1,8 @@
+import WelcomePageContent from "../components/Welcome/WelcomePageContent";
+import React from "react";
+
+const WelcomePage = () => {
+	return <WelcomePageContent />;
+};
+
+export default WelcomePage;
