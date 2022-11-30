0d6b24c3905c0c5202b59bf945aa5d7c71ba3f87
Slicknick300 <116517655+Slicknick300@users.noreply.github.com>
2022-11-30 15:26:21 -0500

    Add files via upload

---
+++ Pages/PasswordChangePage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import UserChangePassword from "../components/Password/UserChangePassword";
+
+const ChangePasswordPage = () => {
+	return <UserChangePassword />;
+};
+
+export default ChangePasswordPage;
